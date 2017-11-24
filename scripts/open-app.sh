ad2cce27569e9b9423a9db0a6009b12434d3aa0d
Brian Gonzalez <me@briangonzalez.org>
2017-11-24 13:13:27 -0800

    open app

--- scripts/open-app.sh
+++ scripts/open-app.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-echo $PWD
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-LINK=$DIR/reminders-menu-bar
-SDIR="$(dirname "$(readlink "$LINK")")"
-echo $SDIR
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # path of this file
+LINK=$DIR/reminders-menu-bar # this is a symlink
+SDIR="$(dirname "$(readlink "$LINK")")" # get full path which link points to
 
 cd $DIR
 APP_PATH="$SDIR/Reminders Menu Bar.app"
-echo $APP_PATH
 open "$APP_PATH"
